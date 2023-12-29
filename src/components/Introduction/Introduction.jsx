d42a0a037057627f732f67732ab263b58ce59fe6
aman-anand-hub <amanngp01@gmail.com>
2023-12-30 03:50:45 +0530

    third commit

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -4,12 +4,12 @@ import styles from "./Introduction.module.css";
 function Introduction() {
 
   return (
+    <div className={styles.container}>
     <div className={styles.wrapper}>
 
         <h1>Let’s Introduce Ourself</h1>
 
         <div className={styles.line}>
-
         </div>
 
         <div className={styles.rightHalf}>
@@ -17,6 +17,7 @@ function Introduction() {
             <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit Exercitation.
             </p>
         </div>
+    </div>
     </div>
   )
 
--- src/components/SwiperComponent/SwiperComponent.jsx
+++ src/components/SwiperComponent/SwiperComponent.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from "./SwiperComponennt.module.css";
-import SwiperButton from './SwiperButton/SwiperButton.jsx';
+import ContentBox from "../ChooseUs/ContentBox/ContentBox.jsx";
+import LeftNavigationButton from './LeftNavigationButton/LeftNavigationButton.jsx';
+import RightNavigationButton from './RightNavigationButton/RightNavigationButton.jsx';
+
+import emp1 from "../../assets/emp1.png";
+import emp2 from "../../assets/emp2.png";
+import emp3 from "../../assets/emp3.png";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Navigation } from "swiper/modules";
+import { useSwiper } from 'swiper/react';
 
 function SwiperComponent() {
+
+    const array= 
+    [
+        <ContentBox>98% success rate</ContentBox>,
+        <ContentBox>98% success rate</ContentBox>,
+        <ContentBox>98% success rate</ContentBox>,
+        <ContentBox>98% success rate</ContentBox>,
+        <ContentBox>98% success rate</ContentBox>,
+        <ContentBox>98% success rate</ContentBox>,
+        <ContentBox>98% success rate</ContentBox>,
+        <ContentBox>98% success rate</ContentBox>,
+        <ContentBox>98% success rate</ContentBox>
+    ];
+
+    const swiper = useSwiper();
+
+    const Controls= ({arr}) => {
+        const { swiper }= useSwiper();
+        console.log(swiper);
+        useEffect(() => {
+            swiper?.slideTo(0);
+        }, [arr]);
+        return <></>
+    }
+
   return (
     <div className={styles.wrapper}>
 
@@ -10,23 +46,40 @@ function SwiperComponent() {
 
             <div className={styles.header}>
 
-                <p>what says our happy Clients</p>
-
-                <div className={styles.btnSection}>
-                    <SwiperButton type={"left"} />
-                    <SwiperButton type={"right"}/>
-                </div>
+                <p>What says our happy Clients</p>
 
             </div>
 
-            <div className={styles.carouselDiv}>
-                
-            </div>
+            <div>
+                <Swiper
+                  modules={{ Navigation }}
+                  spaceBetween={40}
+                  slidesPerView={3}
+                  style={{padding: "20px 30px"}}
+                  initialSlide={0}
+                  allowTouchMove
+                >  
+                    <Controls arr={array}/>
 
+                    <div className={styles.buttonDiv}>
+                        <LeftNavigationButton />
+                        <RightNavigationButton />
+                    </div>
+                    
+
+                    {array.map((item, index) => (
+                        <SwiperSlide key={index}>
+                         {item}
+                        </SwiperSlide>
+                    ))}
+
+                </Swiper>
+            </div>
+            
         </div>
 
     </div>
   )
 }
 
-export default SwiperComponent
+export default SwiperComponent;
